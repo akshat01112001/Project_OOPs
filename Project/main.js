585ce9a609e3c582576cc95c12a2dedacf629251
Akshat Kumar <akshatkumar.2001@gmail.com>
2021-11-12 15:01:53 +0530

    Update main.js

--- Project/main.js
+++ Project/main.js
@@ -1,1 +1,20 @@
 document.write("<h1>Parking</h1>");
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from "firebase/database";
+
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+const firebaseConfig = {
+  apiKey: "apiKey",
+  authDomain: "projectId.firebaseapp.com",
+  // For databases not in the us-central1 location, databaseURL will be of the
+  // form https://[databaseName].[region].firebasedatabase.app.
+  // For example, https://your-database-123.europe-west1.firebasedatabase.app
+  databaseURL: "https://databaseName.firebaseio.com",
+  storageBucket: "bucket.appspot.com"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+const database = getDatabase(app);
