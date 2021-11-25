bbf43a0be5e94f6acf946ae44e4578625de610c3
Akshat Kumar <akshatkumar.2001@gmail.com>
2021-11-25 11:46:39 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,5 +1,6 @@
-const input = document.querySelectorAll("input");
+let date = document.getElementById('date').value;
+let check_in_time = document.getElementById('check-in time').value;
+let check_out_time = document.getElementById('check_out_time').value;
+let phone_no = document.getElementById('phone no').value;
 
-input.forEach(i=> {
-    console.log("done");
-});
+alert(date+' '+check_in_time+' '+check_out_time+' '+phone_no+' ')
