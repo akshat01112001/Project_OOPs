a5da854765afb0126a5530235c134373d51c33c8
Akshat Kumar <akshatkumar.2001@gmail.com>
2021-11-25 13:31:01 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,8 +1,8 @@
 
-var location = document.querySelector('#location').value;
-var date = document.querySelector('#date').value;
-var check_in_time = document.querySelector('#check_in_time').value;
-var check_out_time = document.querySelector('#check_out_time').value;
-var phone_no = document.querySelector('#phone_no').value;
+var location = document.getElementById('location').value;
+var date = document.getElementById('date').value;
+var check_in_time = document.getElementById('check_in_time').value;
+var check_out_time = document.getElementById('check_out_time').value;
+var phone_no = document.getElementById('phone_no').value;
 
 alert(date + ' ' + check_in_time + ' ' + check_out_time + ' ' + phone_no);
