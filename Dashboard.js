510570e9f3b8f7df40ad61056836034a176cfa2b
Akshat Kumar <akshatkumar.2001@gmail.com>
2021-12-03 11:31:15 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,8 +1,75 @@
+const container = document.querySelector(".container");
+const slots = document.querySelectorAll(".row .slot:not(.sold)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const slotSelect = document.getElementById("slot");
 
-var location = document.getElementById('location').value;
-var date = document.getElementById('date').value;
-var check_in_time = document.getElementById('check_in_time').value;
-var check_out_time = document.getElementById('check_out_time').value;
-var phone_no = document.getElementById('phone_no').value;
+populateUI();
 
-alert(date + ' ' + check_in_time + ' ' + check_out_time + ' ' + phone_no);
+let ticketPrice = +slotSelect.value;
+
+// Save selected slot index and price
+function setSlotData(slotIndex, slotPrice) {
+  localStorage.setItem("selectedSlotIndex", slotIndex);
+  localStorage.setItem("selectedSlotPrice", slotPrice);
+}
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSlots = document.querySelectorAll(".row .slot.selected");
+
+  const slotsIndex = [...selectedSlots].map((slot) => [...slots].indexOf(slot));
+
+  localStorage.setItem("selectedslots", JSON.stringify(slotsIndex));
+
+  const selectedSlotsCount = selectedSlots.length;
+
+  count.innerText = selectedSlotsCount;
+  total.innerText = selectedSlotsCount * ticketPrice;
+
+  setSlotData(slotSelect.selectedIndex, slotSelect.value);
+}
+
+
+// Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSlots = JSON.parse(localStorage.getItem("selectedSlots"));
+
+  if (selectedSlots !== null && selectedSlots.length > 0) {
+    slots.forEach((slot, index) => {
+      if (selectedSlots.indexOf(index) > -1) {
+        slot.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedSlotIndex = localStorage.getItem("selectedSlotIndex");
+
+  if (selectedSlotIndex !== null) {
+    slotSelect.selectedIndex = selectedSlotIndex;
+  }
+}
+
+// Slot select event
+slotSelect.addEventListener("change", e => {
+  ticketPrice = +e.target.value;
+  setSlotData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// Slot click event
+container.addEventListener("click", e => {
+  if (
+    e.target.classList.contains("slot") &&
+    !e.target.classList.contains("sold")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
+
+// Initial count and total set
+updateSelectedCount();
+
+//alert(date + ' ' + check_in_time + ' ' + check_out_time + ' ' + phone_no);
