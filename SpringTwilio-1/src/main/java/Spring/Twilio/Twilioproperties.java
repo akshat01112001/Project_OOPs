b7df21fd065d142384752c5fe2897eb60c5f0f11
ImtheDM <87141930+ImtheDM@users.noreply.github.com>
2021-12-04 03:23:59 +0530

    Add files via upload

---
+++ SpringTwilio-1/src/main/java/Spring/Twilio/Twilioproperties.java
@@ -0,0 +1,36 @@
+package Spring.Twilio;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("twilio")
+public class Twilioproperties {
+	
+	private String accountSid;
+	private String authToken;
+	private String serviceId;
+	
+	public Twilioproperties() {
+		
+	}
+	public String getAccountSid() {
+		return accountSid;
+	}
+	public void setAccountSid(String accountSid) {
+		this.accountSid = accountSid;
+	}
+	public String getAuthToken() {
+		return authToken;
+	}
+	public void setAuthToken(String authToken) {
+		this.authToken = authToken;
+	}
+	public String getServiceId() {
+		return serviceId;
+	}
+	public void setServiceId(String serviceId) {
+		this.serviceId = serviceId;
+	}
+
+}
---
+++ SpringTwilio-1/src/test/java/Spring/Twilio/SpringTwilio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Twilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTwilio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
