f28df07169e218a6386034459acfa0bd04feddbf
ImtheDM <87141930+ImtheDM@users.noreply.github.com>
2021-12-05 02:14:09 +0530

    Add files via upload

---
+++ Spring_Twilio_Otp_Validate-main (2)/Spring_Twilio_Otp_Validate-main/SpringTwilio/src/main/java/Spring/Twilio/SpringTwilioApplication.java
@@ -0,0 +1,16 @@
+package Spring.Twilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+
+@SpringBootApplication
+public class SpringTwilioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTwilioApplication.class, args);
+	}
+
+}
