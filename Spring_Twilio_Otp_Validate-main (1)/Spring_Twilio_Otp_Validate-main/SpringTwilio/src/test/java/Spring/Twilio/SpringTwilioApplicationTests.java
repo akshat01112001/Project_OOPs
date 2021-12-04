d6f73fd6096e201e3441bf379c5d15f9cfa66192
ImtheDM <87141930+ImtheDM@users.noreply.github.com>
2021-12-04 18:11:56 +0530

    Add files via upload

---
+++ Spring_Twilio_Otp_Validate-main (1)/Spring_Twilio_Otp_Validate-main/SpringTwilio/src/test/java/Spring/Twilio/SpringTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Twilio;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTwilioApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
